2119e19b54f9ba1c02f0c67f5598ea20d02c5667
Darell <darell691@gmail.com>
2025-08-10 23:35:26 -0400

    Fixed HomePage and sign in styling. Also fixed backend.

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -4,22 +4,24 @@ const PlayerCard = ({ player }) => {
   if (!player) return null;
 
   console.log('Player data in PlayerCard:', player); // Log the player data
-  const playerStats = player[player.length - 1]; // Get the last element of the array
+  const playerStats = player.body[player.body.length - 1]; // Get the last element of the array
 //HEADSHOT WORKS, WORKING ON GETTING DATA TO DISPLAY
+
+console.log('Player stats:', playerStats);
   return (
     <div>
-      <h2>{player.name}</h2>
+      <h2>{playerStats.name}</h2>
       <img src={player.headshotUrl} alt={player.name} width="200" />
-      <p>Team: {player.team}</p>
-      <p>Position: {player.position}</p>
-      <p>Points per game: {player.pointsPerGame}</p>
-      <p>Assists per game: {player.assistsPerGame}</p>
-      <p>Rebounds per game: {player.totalReboundsPerGame}</p>
-      <p>Steals per game: {player.stealsPerGame}</p>
-      <p>Blocks per game: {player.blocksPerGame}</p>
-      <p>Field Goal Percentage: {player.fieldGoalPercentage}</p>
-      <p>Three Point Percentage: {player.threePointFieldGoalPercentage}</p>
-      <p>Free Throw Percentage: {player.freeThrowPercentage}</p>
+      <p>Team: {playerStats.team}</p>
+      <p>Position: {playerStats.position}</p>
+      <p>Points per game: {playerStats.pointsPerGame}</p>
+      <p>Assists per game: {playerStats.assistsPerGame}</p>
+      <p>Rebounds per game: {playerStats.totalReboundsPerGame}</p>
+      <p>Steals per game: {playerStats.stealsPerGame}</p>
+      <p>Blocks per game: {playerStats.blocksPerGame}</p>
+      <p>Field Goal Percentage: {playerStats.fieldGoalPercentage}</p>
+      <p>Three Point Percentage: {playerStats.threePointFieldGoalPercentage}</p>
+      <p>Free Throw Percentage: {playerStats.freeThrowPercentage}</p>
     </div>
   );
 };
